a8b729b15eccbb50c8c1b92ac949ce847f0c3892
Aarna Tejwani <aarnatejwani@gmail.com>
2026-05-11 22:54:59 +0530

    updated relative path in loan_analysis.py

--- loan_analysis.py
+++ loan_analysis.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent
 
-df = pd.read_csv("datasets/dataset-1.csv")
+file_path = BASE_DIR / "results" / "final_predictions.csv"
+
+df = pd.read_csv(file_path)
 
 print(df.head())
 print(df.info())
